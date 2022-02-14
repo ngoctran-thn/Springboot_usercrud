975afbfabce28aff1cc69c67662737eb5a4bcdbe
Ngoc Tran <tranthainhien1603@gmail.com>
2022-02-14 23:19:46 +0200

    Post new user, return error when profile id not found

--- src/main/java/com/ngoc/usercrud/controllers/UserController.java
+++ src/main/java/com/ngoc/usercrud/controllers/UserController.java
@@ -2,11 +2,12 @@ package com.ngoc.usercrud.controllers;
 
 import com.ngoc.usercrud.entities.Profile;
 import com.ngoc.usercrud.entities.User;
+import com.ngoc.usercrud.repositories.ProfileRepository;
 import com.ngoc.usercrud.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -14,6 +15,8 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ProfileRepository profileRepository;
 
     //Try http://localhost:8080/users
     @GetMapping("/users")
@@ -24,6 +27,15 @@ public class UserController {
     @GetMapping("/user/{id}")
     public Optional<User> getUser(@PathVariable("id") int id){
         return userRepository.findById(id);
+    }
+    @PostMapping("/user")
+    public ResponseEntity<?> save(@RequestBody User user) {
+        Optional<Profile> optionalProfile = profileRepository.findById(user.getProfile().getId());
+        if (optionalProfile.isEmpty()) {
+            return new ResponseEntity<>("Profile ID " + user.getProfile().getId() + " not found.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        user.setProfile(optionalProfile.get());
+        return new ResponseEntity<User>(userRepository.save(user), HttpStatus.OK);
     }
 
 }
