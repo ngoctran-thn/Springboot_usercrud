736aec957fc32892f0e165286bfe12b02cd18819
Ngoc Tran <tranthainhien1603@gmail.com>
2022-02-14 22:42:01 +0200

    Get user with proflie

--- src/main/java/com/ngoc/usercrud/controllers/UserController.java
+++ src/main/java/com/ngoc/usercrud/controllers/UserController.java
@@ -1,10 +1,14 @@
 package com.ngoc.usercrud.controllers;
 
+import com.ngoc.usercrud.entities.Profile;
 import com.ngoc.usercrud.entities.User;
 import com.ngoc.usercrud.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -16,4 +20,10 @@ public class UserController {
     public Iterable<User> getUsers(){
         return userRepository.findAll();
     }
+    //Try http://localhost/user/2
+    @GetMapping("/user/{id}")
+    public Optional<User> getUser(@PathVariable("id") int id){
+        return userRepository.findById(id);
+    }
+
 }
