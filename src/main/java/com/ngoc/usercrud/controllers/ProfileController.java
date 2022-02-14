f59cd68edd6337fe62441a165e069ae10814830a
Ngoc Tran <tranthainhien1603@gmail.com>
2022-02-14 22:08:27 +0200

    Finish Profile endpoint

--- src/main/java/com/ngoc/usercrud/controllers/ProfileController.java
+++ src/main/java/com/ngoc/usercrud/controllers/ProfileController.java
@@ -3,8 +3,9 @@ package com.ngoc.usercrud.controllers;
 import com.ngoc.usercrud.entities.Profile;
 import com.ngoc.usercrud.repositories.ProfileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class ProfileController {
@@ -14,7 +15,32 @@ public class ProfileController {
 
     //Try http://localhost:8080/profiles
     @GetMapping("/profiles")
-    public Iterable<Profile> getPersons(){
+    public Iterable<Profile> getprofles(){
         return profileRepository.findAll();
+    }
+
+    //Try http://localhost/profile/2
+    @GetMapping("/profile/{id}")
+    public Optional<Profile> getprofle(@PathVariable("id") int id){
+        return profileRepository.findById(id);
+    }
+
+    @PostMapping("/profile")
+    //Use postman and url http://localhost/profle, method POST and put {"name":"Test"} in body
+    public Profile save(@RequestBody Profile profile){
+        return profileRepository.save(profile);
+    }
+
+    //update = save
+    @PutMapping("/profile")
+    //Use postman and url http://localhost/profle, method PUT and put {"id":6, "name":"Test2"} in body
+    public Profile update(@RequestBody Profile profile){
+        return profileRepository.save(profile);
+    }
+
+    @DeleteMapping("/profile")
+    //Use postman and url http://localhost/profle, method DELETE and put {"id":6} in body
+    public void delete(@RequestBody Profile profile){
+        profileRepository.delete(profile);
     }
 }
